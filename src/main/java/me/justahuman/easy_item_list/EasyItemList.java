eb91d09ca4ffe7b99addd89eb4aa4991ec4e8737
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-05-08 12:06:51 -0500

    add icon, wip rei stuff

--- src/main/java/me/justahuman/easy_item_list/EasyItemList.java
+++ src/main/java/me/justahuman/easy_item_list/EasyItemList.java
@@ -1,9 +1,20 @@
 package me.justahuman.easy_item_list;
 
+import dev.architectury.event.events.client.ClientRecipeUpdateEvent;
+import me.justahuman.easy_item_list.hooks.ReiHook;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
-// In case I need this later
 public class EasyItemList implements ClientModInitializer {
     @Override
-    public void onInitializeClient() {}
+    public void onInitializeClient() {
+        ClientRecipeUpdateEvent.EVENT.register(manager -> {
+            if (FabricLoader.getInstance().isModLoaded("roughlyenoughitems")) {
+                final ReiHook hook = ReiHook.getInstance();
+                if (hook != null) {
+                    hook.load();
+                }
+            }
+        });
+    }
 }
--- src/main/java/me/justahuman/easy_item_list/hooks/ReiHook.java
+++ src/main/java/me/justahuman/easy_item_list/hooks/ReiHook.java
@@ -5,38 +5,49 @@ import it.unimi.dsi.fastutil.longs.LongSet;
 import me.justahuman.easy_item_list.api.Hook;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.entry.EntryRegistry;
-import me.shedaniel.rei.api.common.entry.EntryStack;
+import me.shedaniel.rei.api.common.entry.comparison.ComparisonContext;
+import me.shedaniel.rei.api.common.entry.comparison.EntryComparator;
 import me.shedaniel.rei.api.common.entry.comparison.ItemComparatorRegistry;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.minecraft.item.ItemStack;
 
 public class ReiHook extends Hook implements REIClientPlugin {
     private static final LongSet STACK_HASHES = new LongOpenHashSet();
-    private EntryRegistry registry;
+    private static ReiHook instance = null;
+    private EntryRegistry registry = null;
+
+    public ReiHook() {
+        instance = this;
+    }
 
     @Override
     public void registerEntries(EntryRegistry registry) {
         this.registry = registry;
-        load();
     }
 
     @Override
     public void registerItemComparators(ItemComparatorRegistry registry) {
         registry.registerGlobal((context, stack) -> {
-            final long exact = EntryStacks.hashExact(EntryStacks.of(stack));
-            return STACK_HASHES.contains(exact) ? exact : 0;
+            final long hash = EntryComparator.itemComponents().hash(ComparisonContext.EXACT, stack);
+            return STACK_HASHES.contains(hash) ? hash : 0;
         });
     }
 
     @Override
     public boolean alreadyAdded(ItemStack itemStack) {
-        return this.registry.alreadyContain(EntryStacks.of(itemStack));
+        return this.registry != null && this.registry.alreadyContain(EntryStacks.of(itemStack));
     }
 
     @Override
     public void addItemStack(ItemStack itemStack) {
-        final EntryStack<?> stack = EntryStacks.of(itemStack);
-        this.registry.addEntries(stack);
-        STACK_HASHES.add(EntryStacks.hashExact(stack));
+        if (this.registry == null) {
+            return;
+        }
+        this.registry.addEntries(EntryStacks.of(itemStack));
+        STACK_HASHES.add(EntryComparator.itemComponents().hash(ComparisonContext.EXACT, itemStack));
+    }
+
+    public static ReiHook getInstance() {
+        return instance;
     }
 }
